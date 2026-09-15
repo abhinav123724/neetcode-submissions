0d68410e8ae0b3f66124542f5fd3797a6daf7e9d
abhinav123724 <nuerox04@gmail.com>
2026-09-15 22:48:57 +0530

    Add: find-pivot-index - submission-0

---
+++ Data Structures & Algorithms/find-pivot-index/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int left=0;
+        int total=0;
+        for(int i=0;i<nums.length;i++){
+            total=nums[i]+total;
+
+        }
+        for(int i=0;i<nums.length;i++){
+            int right=total-left-nums[i];
+            if(left==right){
+                return i;
+            }
+            else{
+                left=left+nums[i];
+            }
+        }
+        
+        return -1;
+    }
+}
